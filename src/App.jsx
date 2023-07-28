cc47529dc30309d493043b6bb9663e6a09a03623
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-07-28 19:35:23 +0200

    app terminé, commencement de la gestion d'erreur

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
+import { useEffect, useState } from "react";
 import loader from "./assets/loader.svg";
-import "./App.css"
+import browser from "./assets/browser.svg";
+import "./App.css";
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
+  const [weatherData, setWheatherData] = useState(null);
+  const [errorInfo, SetErrorInfo] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log(responseData);
+        setWheatherData({
+          city: responseData.data.city,
+          country: responseData.data.country,
+          iconId: responseData.data.current.weather.ic,
+          temperature: responseData.data.current.weather.tp,
+        });
+      });
+  }, []);
+
   return (
     <main>
-      <div className="loader-container">
-        <img src={loader} alt="loeading icon" />
+      <div className={`loader-container ${!weatherData && "active"}`}>
+        <img src={loader} alt="loading icon" />
       </div>
-      <p className="city-name">Paris</p>
-      <p className="country-name">France</p>
-      <p className="temperature">17°</p>
-      <div className="info-icon-container">
-        <img src="/icons/01d.svg" className="info-icon" alt="weather icon" />
-      </div>
+
+      {weatherData && (
+        <>
+          <p className="city-name">{weatherData.city}</p>
+          <p className="country-name">{weatherData.country}</p>
+          <p className="temperature">{weatherData.temperature} °</p>
+          <div className="info-icon-container">
+            <img
+              src="/icons/01d.svg"
+              className="info-icon"
+              alt="weather icon"
+            />
+          </div>
+        </>
+      )}
+
+      {errorInfo && !weatherData && (
+        <>
+          <p className="error-information">
+            {errorInfo.msg}
+            <img src={browser} alt="error icon" />
+          </p>
+        </>
+      )}
     </main>
   );
 }
